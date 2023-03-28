faa054f10fde455d32c04dda096cdc3bfd50c91f
CryptoSnowPrince <topdirector2017@gmail.com>
2023-03-28 17:50:28 +0900

    init

---
+++ scripts/networks/chain.js
@@ -0,0 +1,154 @@
+const chainInfo = {
+    core: {
+        gmx: {
+            name: "CRUST",
+            symbol: "CRUST"
+        },
+        esgmx: {
+            name: "Escrowed CRUST",
+            symbol: "zCRUST"
+        },
+        esgmxiou: {
+            name: "zCRUST IOU",
+            symbol: "zCRUST:IOU"
+        },
+        bngmx: {
+            name: "Bonus CRUST",
+            symbol: "bnCRUST"
+        },
+        glp: {
+            name: "CRUST LP",
+            symbol: "CLP"
+        },
+        sgmx: {
+            name: "Staked CRUST",
+            symbol: "sCRUST",
+        },
+        sbgmx: {
+            name: "Staked + Bonus CRUST",
+            symbol: "sbCRUST",
+        },
+        sbfgmx: {
+            name: "Staked + Bonus + Fee CRUST",
+            symbol: "sbfCRUST"
+        },
+        fglp: {
+            name: "Fee CLP",
+            symbol: "fCLP"
+        },
+        fsglp: {
+            name: "Fee + Staked CLP",
+            symbol: "fsCLP"
+        },
+        vgmx: {
+            name: "Vested CRUST",
+            symbol: "vCRUST"
+        },
+        vglp: {
+            name: "Vested CLP",
+            symbol: "vCLP"
+        }
+    },
+    bsctestnet: {
+        gmx: {
+            name: "Mantis",
+            symbol: "MANTIS"
+        },
+        esgmx: {
+            name: "Escrowed Mantis",
+            symbol: "zMANTIS"
+        },
+        esgmxiou: {
+            name: "zMANTIS IOU",
+            symbol: "zMANTIS:IOU"
+        },
+        bngmx: {
+            name: "Bonus Mantis",
+            symbol: "bnMANTIS"
+        },
+        glp: {
+            name: "Mantis LP",
+            symbol: "MLP"
+        },
+        sgmx: {
+            name: "Staked Mantis",
+            symbol: "sMANTIS",
+        },
+        sbgmx: {
+            name: "Staked + Bonus Mantis",
+            symbol: "sbMANTIS",
+        },
+        sbfgmx: {
+            name: "Staked + Bonus + Fee Mantis",
+            symbol: "sbfMANTIS"
+        },
+        fglp: {
+            name: "Fee MLP",
+            symbol: "fMLP"
+        },
+        fsglp: {
+            name: "Fee + Staked MLP",
+            symbol: "fsMLP"
+        },
+        vgmx: {
+            name: "Vested Mantis",
+            symbol: "vMANTIS"
+        },
+        vglp: {
+            name: "Vested MLP",
+            symbol: "vMLP"
+        }
+    },
+    shibarium: {
+        gmx: {
+            name: "Mantis",
+            symbol: "MANTIS"
+        },
+        esgmx: {
+            name: "Escrowed Mantis",
+            symbol: "zMANTIS"
+        },
+        esgmxiou: {
+            name: "zMANTIS IOU",
+            symbol: "zMANTIS:IOU"
+        },
+        bngmx: {
+            name: "Bonus Mantis",
+            symbol: "bnMANTIS"
+        },
+        glp: {
+            name: "Mantis LP",
+            symbol: "MLP"
+        },
+        sgmx: {
+            name: "Staked Mantis",
+            symbol: "sMANTIS",
+        },
+        sbgmx: {
+            name: "Staked + Bonus Mantis",
+            symbol: "sbMANTIS",
+        },
+        sbfgmx: {
+            name: "Staked + Bonus + Fee Mantis",
+            symbol: "sbfMANTIS"
+        },
+        fglp: {
+            name: "Fee MLP",
+            symbol: "fMLP"
+        },
+        fsglp: {
+            name: "Fee + Staked MLP",
+            symbol: "fsMLP"
+        },
+        vgmx: {
+            name: "Vested Mantis",
+            symbol: "vMANTIS"
+        },
+        vglp: {
+            name: "Vested MLP",
+            symbol: "vMLP"
+        }
+    }
+}
+
+module.exports = { chainInfo }
---
+++ scripts/networks/localhost.js
@@ -0,0 +1,46 @@
+const deployTokenManager = require("../access/deployTokenManager");
+const deployOrderBook = require("../core/deployOrderBook");
+const deployPositionManager = require("../core/deployPositionManager");
+const deployPositionRouter = require("../core/deployPositionRouter");
+const deployReferralReader = require("../core/deployReferralReader");
+const deployReferralStorage = require("../core/deployReferralStorage");
+const deployVault = require("../core/deployVault")
+const deployGMX = require("../gmx/deployGMX");
+const deployTokens = require("../gmx/deployTokens");
+const deployWETH = require("../gmx/deployWETH");
+const deployOrderBookReader = require("../peripherals/deployOrderBookReader");
+const deployPriceFeedTimelock = require("../peripherals/deployPriceFeedTimelock");
+const deployReader = require("../peripherals/deployReader");
+const deployRewardReader = require("../peripherals/deployRewardReader");
+const deployShortsTrackerTimelock = require("../peripherals/deployShortsTrackerTimelock");
+const deployTimelock = require("../peripherals/deployTimelock");
+const deployVaultReader = require("../peripherals/deployVaultReader");
+const deployRewardRouterV2 = require("../staking/deployRewardRouterV2");
+const deployPriceFeed = require("../core/deployPriceFeed");
+const { getGasUsed } = require("../shared/syncParams");
+
+const deploy_localhost = async () => {
+  await deployWETH()
+  await deployGMX()
+  await deployVault()
+  await deployVaultReader()
+  await deployReader()
+  await deployRewardReader()
+  await deployTokens()
+  await deployRewardRouterV2()
+  await deployOrderBook()
+  await deployOrderBookReader()
+  await deployReferralStorage()
+  await deployReferralReader()
+  await deployTokenManager()
+  await deployPriceFeedTimelock()
+  await deployTimelock()
+  await deployShortsTrackerTimelock()
+  await deployPositionRouter()
+  await deployPositionManager()
+  await deployPriceFeed()
+
+  console.log('gas used:', getGasUsed())
+};
+
+module.exports = { deploy_localhost };
